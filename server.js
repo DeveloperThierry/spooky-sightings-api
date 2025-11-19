040b67e27133a600eed8bf73010826196a0ff16a
developerthierry <developerthierry@gmail.com>
2025-11-19 01:07:46 +0000

    Relative File Path - Get Path to Resource

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-import http from 'node:http'
-const PORT = 8000
+import http from "node:http";
+import path from "node:path";
+
+const PORT = 8000;
+
+const __dirname = import.meta.dirname;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><h1>The server is working</h1></html>')
-})
+    const absPathToResources = path.join(__dirname, 'public', 'index.html');
+    const relPathToResources = path.join('public', 'index.html');
+    console.log('absolute: ', absPathToResources);
+  console.log('relative: ', relPathToResources);
 
-server.listen(PORT, ()=> console.log('Connected'))
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><h1>The server is working</h1></html>");
+});
+
+server.listen(PORT, () => console.log("Connected"));
