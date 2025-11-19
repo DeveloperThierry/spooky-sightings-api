1bbe57425b76415fa1813b37aca1c38b46290e70
developerthierry <developerthierry@gmail.com>
2025-11-19 01:48:18 +0000

    Async / Promises

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 import http from "node:http";
 import path from "node:path";
+import { serveStatic } from "./utils/serveStatic.js";
+import fs from "node:fs/promises";
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
 
-const server = http.createServer((req, res) => {
-    const absPathToResources = path.join(__dirname, 'public', 'index.html');
-    const relPathToResources = path.join('public', 'index.html');
-    console.log('absolute: ', absPathToResources);
-  console.log('relative: ', relPathToResources);
-
+const server = http.createServer(async (req, res) => {
+  const pathToResource = serveStatic(__dirname);
+  const content = await fs.readFile(pathToResource, "utf8");
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("<html><h1>The server is working</h1></html>");
+  res.end(content);
 });
 
 server.listen(PORT, () => console.log("Connected"));
