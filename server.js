c0999baa5edf9b5cf3a0799cebffd25afe31d011
developerthierry <developerthierry@gmail.com>
2025-11-19 03:12:33 +0000

    Wire Up Data

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ import http from "node:http";
 import path from "node:path";
 import { serveStatic } from "./utils/serveStatic.js";
 import fs from "node:fs/promises";
+import {getContentType} from './utils/getContentType.js'
+import {getData} from './utils/getData.js'
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
-
+console.log(await getData())
 const server = http.createServer(async (req, res) => {
-  const pathToResource = serveStatic(__dirname);
-  const content = await fs.readFile(pathToResource, "utf8");
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(content);
+  await serveStatic(req, res, __dirname);
 });
 
 server.listen(PORT, () => console.log("Connected"));
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,6 +1,27 @@
-import path from 'node:path'
-export function serveStatic(baseDir){
-    const filePath = path.join(baseDir, 'public', 'index.html')
-    console.log(filePath)
-    return filePath
+import path from "node:path";
+import fs from "node:fs/promises";
+import { sendResponse } from "./sendResponse.js";
+import {getContentType} from './getContentType.js'
+
+export async function serveStatic(req, res, baseDir) {
+  const publicDir = path.join(baseDir, "public");
+  const filePath = path.join(publicDir, 
+    req.url === "/" ? 'index.html':req.url
+  )
+  console.log(filePath);
+
+  const ext = path.extname(filePath)
+  const contentType = getContentType(ext)
+
+  try {
+    const content = await fs.readFile(filePath);
+    sendResponse(res, 200, "text/html", content);
+  } catch (e) {
+    if(e.code === 'ENOENT'){
+      const content = await fs.readFile(path.join(publicDir, '404.html'))
+      sendResponse(res, 404, contentType, content)
+    } else {
+      sendResponse(res, 500, 'text/html', `<html><h1>GHOST ERROR CODE BOO ${e.code}</h1></html>`)
+    }
+  }
 }
